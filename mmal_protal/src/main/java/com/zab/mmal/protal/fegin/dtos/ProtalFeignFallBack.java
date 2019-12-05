c7f0b2c853d6a38658cabb14b096f90e79ef8430
zaobo <zhangab1113@163.com>
2019-12-05 21:53:41 +0800

    提交feign回执

--- mmal_protal/src/main/java/com/zab/mmal/protal/fegin/dtos/ProtalFeignFallBack.java
+++ mmal_protal/src/main/java/com/zab/mmal/protal/fegin/dtos/ProtalFeignFallBack.java
@@ -1,11 +1,14 @@
 package com.zab.mmal.protal.fegin.dtos;
 
 import com.zab.mmal.api.dtos.ProductDetails;
+import com.zab.mmal.api.entity.MmallCart;
 import com.zab.mmal.api.entity.MmallUser;
 import com.zab.mmal.common.commons.ReturnData;
 import com.zab.mmal.common.enums.SysCodeMsg;
 import com.zab.mmal.protal.fegin.ProtalFeignService;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ProtalFeignFallBack implements ProtalFeignService {
@@ -57,6 +60,36 @@ public class ProtalFeignFallBack implements ProtalFeignService {
     @Override
     public ReturnData pageProductByCondition(Integer pageNo, Integer pageSize, String keyword, Integer categoryId, String orderBy) {
         return new ReturnData(SysCodeMsg.FAIL);
+    }
+
+    @Override
+    public ReturnData listCart(Integer userId) {
+        return new ReturnData(SysCodeMsg.FAIL);
+    }
+
+    @Override
+    public ReturnData addCart(MmallCart cart) {
+        return new ReturnData(SysCodeMsg.FAIL);
+    }
+
+    @Override
+    public ReturnData updateCart(MmallCart cart) {
+        return new ReturnData(SysCodeMsg.FAIL);
+    }
+
+    @Override
+    public ReturnData deleteCart(Integer userId, List<Integer> productIds) {
+        return new ReturnData(SysCodeMsg.FAIL);
+    }
+
+    @Override
+    public ReturnData selectOrUnSelectCart(Integer userId, Integer checked, List<Integer> productIds) {
+        return new ReturnData(SysCodeMsg.FAIL);
+    }
+
+    @Override
+    public ReturnData getCartProductCount(Integer userId) {
+        return null;
     }
 
 }
